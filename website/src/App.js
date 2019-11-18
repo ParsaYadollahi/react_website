fa3d7bee02ec90632059a30331f9c21e38832fc0
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-11-17 23:10:53 -0500

    basic css

---
+++ website/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  
+  render() {
+    return (
+        <div className="fullpage">
+          <p>yoo</p>
+          <div className="center_div">
+              <img src="https://cdn3.f-cdn.com/contestentries/1376995/30494909/5b566bc71d308_thumb900.jpg"></img>
+          <div className="image_center_div">
+            ABBAS YADOLLAHI
+          </div>
+          <div className="image_center_div_soft">SOFTWARE ENGINEER</div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
