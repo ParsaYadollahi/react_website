6da4e99a165406e7f9c8c25d878e852b570d89e6
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-11-18 23:20:32 -0500

    everything done, small updates left

--- website/src/App.js
+++ website/src/App.js
@@ -1,21 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Email from './email'
+import { Grid, FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
 
 class App extends React.Component {
   
   render() {
     return (
         <div className="fullpage">
-          <p>yoo</p>
-          <div className="center_div">
-              <img src="https://cdn3.f-cdn.com/contestentries/1376995/30494909/5b566bc71d308_thumb900.jpg"></img>
-          <div className="image_center_div">
-            ABBAS YADOLLAHI
-          </div>
-          <div className="image_center_div_soft">SOFTWARE ENGINEER</div>
+            <div className="center_div">
+              <div style={{ height : '100vh'}}>
+                  <img src={require('./pourya_pic.jpg')} className="pourya_image"></img>
+              <div className="image_center_div">
+                  <p>ABBAS YADOLLAHI</p>
+              </div>
+              <div className="image_center_div_soft">
+              <p>SOFTWARE ENGINEER</p>
+              </div>
+            </div>
+          
+          <div className="box">
+            <Grid container spacing={3}>
+              <Grid className="box_bottom_left" item xs={6}>
+              <p className="company">SQUAREPOINT</p>
+                <p className="date">JUNE - PRESENT</p>
+                <p className="company">NATIVE TOUCH</p>
+                <p className="date">MAY - AUG 2018 </p>
+                <p className="company">CISCO</p>
+                <p className="date">MAY - AUG 2017 </p>
+                <p className="company">CISCO</p>
+                <p className="date">MAY - AUG 2016 </p>
+              </Grid>
+
+
+
+
+              <Grid className="box_bottom_right" item xs={6}>
+                <Email />
+              </Grid>
+
+
+
+
+
+
+            </Grid>
           </div>
         </div>
+          
+
+
+
+      </div>
     );
   }
 }
---
+++ website/src/email.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Form, Message } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
+import './email.css'
+
+
+class Email extends React.Component {
+	render() {
+		return(
+			<form action="https://formspree.io/parsa.yadollahi@mail.mcgill.ca" method="POST">
+				<div>
+					<label>NAME</label>
+					<input name="name" placeholder="Name" class="form-control foo" type="text"></input>
+				</div>
+				<div>
+					<label>EMAIL</label>
+					<input type="email" placeholder="Email" class="foo" name="_replyto"></input>
+				</div>
+				<div>
+					<label>SUBJECT</label>
+					<input type="text" placeholder="Subject" class="foo" name="subject"></input>
+				</div>
+				<div>
+					<label>MESSAGE</label>
+					<textarea rows = "4" type="text" placeholder="Message" class="foo" name="message"></textarea>
+				</div>
+				<button>
+					<input className="sumbit" type="submit" value="Submit >>"></input>
+				</button>
+			</form>
+		)
+	}
+}
+
+export default Email;
